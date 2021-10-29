8abfe7c754570a5b14bb557094b5125aa0091c18
Mark Wilber <mqwilber@gmail.com>
2021-10-29 08:49:55 -0400

    Added home range overlap analyses

--- code/calculate_hr_area.R
+++ code/calculate_hr_area.R
@@ -8,11 +8,14 @@ library(sp)
 ##
 ## We then use the Average FOI (converted to a raster) generated from MoveSTIR to estimate
 ## how average FOI on the landscape is distributed within the home range of pigs.
+##
+## Script also calculates and home range overlap between pigs to compare the implications of
+## ignoring fine-scale heterogeneity in space us on epidemilogical dynamics.
 
 dat = fread("../data/pig_movements.csv")
 dat[, datetime:=as.POSIXct(strptime(date_time, format="%Y-%m-%d %H:%M:%S", tz="GMT"))]
 dat[, t:=as.numeric(datetime)]
-unq_collars = unique(dat$individual_ID)
+unq_collars = sort(unique(dat$individual_ID))
 
 # Get home ranges
 home_ranges = array(NA, dim=length(unq_collars))
@@ -54,6 +57,77 @@ for(id in unq_collars){
 	# 80% of the cumulative average FOI occurs in what percent of the home range area?
 	print(id)
 	ratios[[id]] = area_of_high_foi / hr_dat[pig == id]$hr_m2
+}
+
+
+### Calculate home range overlap ###
+
+
+# The first metric of home range overlap we compute is based on the calculation
+# given in Appendix S6 such that the metric is directly comparable to 
+# the direct + indirect MoveSTIR output.  Specifically, we use the area of
+# overlap to directly calculate an FOI between two individuals.
+
+# Get the area of overlap for each pair
+collars = sort(unq_collars[unq_collars != 12])
+area_of_overlap_m2 = array(NA, dim=c(length(collars), length(collars)))
+home_range_mult = array(NA, dim=c(length(collars), length(collars)))
+
+for(i in 1:length(collars)){
+	ci = collars[i]
+	ai = area(my_hrs[[ci]])
+
+	for(j in 1:length(collars)){
+		cj = collars[j]
+		aj = area(my_hrs[[cj]])
+
+		int_shp = intersect(my_hrs[[ci]], my_hrs[[cj]])
+
+		# Check is there is any area of overlap
+		if(is.null(int_shp)){
+			int_area = 0
+		} else {
+			int_area = area(int_shp)
+		}
+		area_of_overlap_m2[i, j] = int_area
+		home_range_mult[i, j] = ai*aj
+
+	}
+}
+
+pathogen_decay = (1 / (24 * 60 * 5)) # 5 days on the minute scale
+
+# You have to scale foi by pathogen decay. See Appendix S6
+foi_scaled = data.table((area_of_overlap_m2 / home_range_mult)) / (pathogen_decay)
+diag(foi_scaled) = 0 # You can't infect yourself, set diagonal to 0
+fwrite(foi_scaled, paste0("../data/home_range_overlaps_", "area", ".csv"))
+
+# Calculate different metrics of overlap.  These are not directly comparable to MoveSTIR
+# because their units are different (they are not hazard rates). 
+# However, we can still ask questions about what they
+# predict regarding how individuals contribute to pathogen spread on the network
+
+home_ranges_trun = home_ranges[unq_collars != 12]
+dat[, Name:=individual_ID]
+trun_dat = dat[individual_ID != 12][, .(Name, UTMx, UTMy)]
+spdat = SpatialPointsDataFrame(trun_dat[, .(UTMx, UTMy)], data=trun_dat[, .(Name)])
+crs(spdat) = "+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+
+# Get utilization distributions on a grid
+kud = kernelUD(spdat[, 1], grid=500, same4all=TRUE)
+
+# Methods are defined in the documentation for kerneloverlaphr
+methods = c("HR", "BA", "VI")
+
+for(method in methods){
+	print(paste("Working on", method))
+	method = method
+	koverlap = kerneloverlaphr(kud, meth=method, conditional=TRUE)
+	diag(koverlap) = 0 # Don't worry about individual's home range overlap with itself
+	koverlap_normed = koverlap 
+	dt_kover = data.table(koverlap_normed)
+
+	fwrite(dt_kover, paste0("../data/home_range_overlaps_", method, ".csv"))
 }
 
 
